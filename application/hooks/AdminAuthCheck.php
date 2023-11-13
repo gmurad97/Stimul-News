1756059874f6bfe876da95b963d55cfc32a78738
gmurad97 <murad.dev@bk.ru>
2023-11-13 04:28:15 +0400

    fix admin auth check hook

--- application/hooks/AdminAuthCheck.php
+++ application/hooks/AdminAuthCheck.php
@@ -7,28 +7,36 @@ class AdminAuthCheck
     {
         $CI = &get_instance();
         $current_route = $CI->uri->uri_string();
-        $allowed_page = array(
+        $allowed_routes = [
             "admin/auth",
-            "admin/register"
-        );
-        if (!$this->isAuthenticated() && !in_array($current_route, $allowed_page, TRUE)) {
-            $CI->session->set_flashdata(
-                "crud_alert",
-                [
-                    "alert_type"          => "danger",
-                    "alert_icon"          => "bi bi-exclamation-octagon",
-                    "alert_bg_color"      => "background-color:rgba(45, 0, 0, 0.32)",
-                    "alert_short_message" => "Danger!",
-                    "alert_long_message"  => "You are not logged in."
-                ]
-            );
-            redirect(base_url("admin/auth"));
+            "admin/auth-action",
+            "admin/register",
+            "admin/register-action"
+        ];
+        if (!$this->isAuthenticated()) {
+            if ($current_route != "admin/auth" && !in_array($current_route, $allowed_routes)) {
+                $CI->session->set_flashdata(
+                    "crud_alert",
+                    [
+                        "alert_type"          => "danger",
+                        "alert_icon"          => "bi bi-exclamation-octagon",
+                        "alert_bg_color"      => "background-color:rgba(45, 0, 0, 0.32)",
+                        "alert_short_message" => "Danger!",
+                        "alert_long_message"  => "You are not logged in."
+                    ]
+                );
+                redirect(base_url("admin/auth"));
+            }
+        } else if ($this->isAuthenticated()) {
+            if (in_array($current_route, $allowed_routes)) {
+                redirect(base_url("admin/dashboard"));
+            }
         }
     }
 
     public function isAuthenticated()
     {
         $CI = &get_instance();
-        return get_instance()->session->has_userdata("admin_auth");
+        return $CI->session->has_userdata("admin_auth");
     }
 }
